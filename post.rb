571b039a76703907db052e73672891387b4a476d
mrYakamoto <wehmenstein@gmail.com>
2015-12-09 16:48:55 -0800

    nokogiri scraping

--- post.rb
+++ post.rb
@@ -1,3 +1,37 @@
 class Post
 
+  def initialize(parsed_html_object)
+    @title = parsed_html_object.post_title
+    @url = parsed_html_object.post_url
+    @points = parsed_html_object.post_points
+    @author_username = parsed_html_object.post_author
+    @item_id = parsed_html_object.post_id
+    @comments = parsed_html_object.comments
+    @comment_authors = parsed_html_object.comment_authors
+  end
+
+
+  def comments
+    puts "\n\tComments:"
+    @comments.each_with_index do |comment, index|
+      puts "\n" + comment
+      puts "\t-" + @comment_authors[index]
+    end
+  end
+
+  def add_comment(comment, author)
+    @comments << comment
+    @comment_authors << author
+  end
+
+  def print_post_with_comments
+    puts
+    print @title + "(Hacker News ID:)" + @item_id +"\n"
+    puts "\tURL: " + @url
+    puts "\tAuthor: " + @author_username
+    puts "\tPoints: " + @points
+    puts
+    comments
+  end
+
 end
--- runner.rb
+++ runner.rb
@@ -3,5 +3,23 @@
 require_relative 'html_whitespace_cleaner'
 require_relative 'post'
 require_relative 'comment'
+require_relative 'hacker_news_parser.rb'
 
 require 'nokogiri'
+require 'open-uri'
+
+# parsed_example_page = HackerNewsParser.new('html-samples/hacker-news-post.html')
+# post = Post.new(parsed_example_page)
+# post.print_post_with_comments
+
+html_file = open('https://news.ycombinator.com/item?id=5003980')
+live_page_parsed = HackerNewsParser.new(html_file)
+live_page_post = Post.new(live_page_parsed)
+live_page_post.print_post_with_comments
+
+
+
+
+
+
+
